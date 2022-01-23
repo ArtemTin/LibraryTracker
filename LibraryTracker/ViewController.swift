0420fa50e5e24263801d244cfa8960427f93df69
Artem Tin <tinartem@gmail.com>
2022-01-23 14:48:20 +0300

    UI

--- LibraryTracker/ViewController.swift
+++ LibraryTracker/ViewController.swift
@@ -1,14 +1,20 @@
 import UIKit
 
-class ViewController: UIViewController {
+class BookDetailViewController: UIViewController {
 
+    @IBOutlet weak var bookName: UILabel!
+    @IBOutlet weak var bookAuthor: UILabel!
+    @IBOutlet weak var bookYear: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var bookShelf = BookShelf(books: [], creator: -1, id: UUID())
+        var bookShelf = BookShelf(books: [], creator: UUID(), id: UUID())
         bookShelf.books.append(Book(name: "A Game of Thrones", authors: [Author(fullName: Name(familyName: "Martin", firstName: "George", middleName: "Raymod Richard"), additionalInfo: ["Birth Year": "1948"], id: UUID())], year: 1996, id: UUID(), genres: ["Fantasy"], bookRegData: nil, anotherVersions: [], rating: 7, progress: BookProgress(pagesRead: 0, pagesInBook: 1567), lastOpenedAt: nil, readingPosition: nil))
         // Do any additional setup after loading the view.
+        
+        bookName.text = bookShelf.books[0].name
+        bookAuthor.text = bookShelf.books[0].authors[0].fullName.familyName + " " + bookShelf.books[0].authors[0].fullName.firstName
+        bookYear.text = String(bookShelf.books[0].year)
     }
-
-
 }
 
